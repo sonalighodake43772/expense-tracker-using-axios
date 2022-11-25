bdbab63d30002aa01bd5658a595746feefff2cc2
sonalighodake43772 <113333875+sonalighodake43772@users.noreply.github.com>
2022-11-25 12:59:29 +0530

    Update expenseAsync.js

--- expenseAsync.js
+++ expenseAsync.js
@@ -1,117 +1,92 @@
-const form=document.getElementById('my-form');
-const amounts=document.getElementById('amount');
-const descriptions=document.getElementById('des');
-const cate=document.getElementById('cat');
+var form = document.getElementById("my-form")
+form.addEventListener("submit",onsubmit)
 
-form.addEventListener('submit',onsubmit);
-
-
-async function onsubmit(e)
-{
-    try{
-    e.preventDefault();
-
-    let expensedetail=
-    {
-        amount:amounts.value,
-        description:descriptions.value,
-        category:cate.value,
-    }
-
-
-
-    axios.post('https://crudcrud.com/api/258282deff7c42e8a17f1d114a41add2/expensetracker',expensedetail)
+async function onsubmit(e){
     
-    showexpense(expensedetail)
-}catch(err)
+    e.preventDefault()
+    var amount = document.getElementById("amount").value
+    var description = document.getElementById("des").value
+    var category = document.getElementById("cat").value
+    
+    const expenseDetails = {
+        amount,
+        description,
+        category
+    }
+try{
+   let response= await axios.post("https://crudcrud.com/api/7d97b926eb69492da7a0af85d6afb62c/expense",expenseDetails)
+    {
+        
+            showNewExpenseOnScreen(response.data);
+            console.log(response);
+ }
+    }catch(err){
+            console.log(err);
+        }
+}
+
+window.addEventListener("DOMContentLoaded",async()=>{
+    try
+    {
+   let Response= await axios.get("https://crudcrud.com/api/7d97b926eb69492da7a0af85d6afb62c/expense")
+       {
+            for(let i=0 ; i<Response.data.length ; i++){
+                console.log(Response)
+                showNewExpenseOnScreen(Response.data[i])
+
+            }
+        }
+    }catch(err)
         {
             console.log(err);
         }
-
-
-
-}
-
-window.addEventListener('DOMContentLoaded',()=>
-{
-    axios.get('https://crudcrud.com/api/258282deff7c42e8a17f1d114a41add2/expensetracker')
-    .then((res)=>
-    {
-        console.log(res);
-        for(var i=0;i<res.data.length;i++)
-        {
-            showexpense(res.data[i]);
-            
-        }
-    })
-
-
+       
 })
-function showexpense(expense)
-{
-    
-    const parentnode=document.getElementById('users');
-    const childnode=`<li id=${expense._id}>${expense.amount}:${expense.description}:${expense.category}
-<button onclick="editExpense('${expense.amount}','${expense.description}','${expense.category}','${expense._id}')">EDIT</button>
-<button onclick="deleteExpense('${expense._id}')">DELETE</button>
-    </li>`;
-    parentnode.innerHTML=parentnode.innerHTML+childnode;
 
-    amounts.value='';
-    descriptions.value='';
-    cate.value='';
+function showNewExpenseOnScreen(user){
+    var parentNode = document.getElementById("users");
+    var childHTML = `<li id = ${user._id}> ${user.amount} - ${user.description} -${user.category} 
+                    <button onclick = deleteExpense('${user._id}')>Delete Expense</button>
+                    <button onclick = editExpense('${user._id}','${user.amount}','${user.description}','${user.category}')>Edit Expense</button>
+                    </li>`
+    parentNode.innerHTML = parentNode.innerHTML + childHTML  
 
+    // clear fields
+    document.getElementById("amount").value='';
+     document.getElementById("des").value='';
+     document.getElementById("cat").value='';
+   
 }
 
-async function deleteExpense(expenseid)
-{
-    try
-    {
- axios.delete(`https://crudcrud.com/api/258282deff7c42e8a17f1d114a41add2/expensetracker/${expenseid}`) 
- 
- {
+async function deleteExpense(id){
+    try{
 
-    removefromsceen(expenseid);
- }
-}catch(err)
+    
+    axios.delete(`https://crudcrud.com/api/7d97b926eb69492da7a0af85d6afb62c/expense/${id}`)
     {
-        console.log(err);
+       
+            removeExpense(id)
+            // console.log(Response)
+        }
+    }catch(err){
+            console.log(err)
+        }
+}
+
+function removeExpense(id){
+    var parentNode = document.getElementById("users")
+    var childnode = document.getElementById(id)
+    if(childnode){
+        parentNode.removeChild(childnode)
     }
-
-
 }
-
- async function editExpense(amount,description,category,exid)
-{
-    try
-    {
-    axios.get(`https://crudcrud.com/api/258282deff7c42e8a17f1d114a41add2/expensetracker/${exid}`) 
-
- {
+ function editExpense(id,amount,description,category){
     
-    document.getElementById('amount').value=amount;
-    document.getElementById('des').value=description;
-    document.getElementById('cat').value=category;
 
-    deleteExpense(exid);
- }
-}catch(err)
-    {
-        console.log(err);
+    document.getElementById("amount").value = amount
+    document.getElementById("des").value = description
+    document.getElementById("cat").value = category;
+      removeExpense(id)
     }
-    
-
-
-}
-
-
-
-function removefromsceen(expenseid)
-{
-const parentnode= document.getElementById('users');
-   const childnode=document.getElementById(expenseid);
-   if(childnode)
-   {
-    parentnode.removeChild(childnode);
-   }
-}
+   
+   
